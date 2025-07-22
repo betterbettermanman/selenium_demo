4b925dac96e528b040b3569338ee05e311aa6d16
czq <1063815316@qq.com>
2025-07-22 16:30:33 +0800

    init3

---
+++ test/selenium_script.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')  # 无头模式
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+
+driver = webdriver.Chrome(options=chrome_options)
+driver.get("https://example.com")
+driver.save_screenshot("screenshot.png")
+print("页面截图已保存")
+driver.quit()
